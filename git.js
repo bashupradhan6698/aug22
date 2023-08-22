ea703ed4282de56814d79a3ffd26a1ffc62525be
Bashu <bashupradhan54@gmail.com>
2023-08-22 21:33:02 +0545

    updated aug22

---
+++ git.js
@@ -0,0 +1,20 @@
+first time
+// git init
+// git remote add origin
+
+--- update code
+
+// 1.  git status 
+// to check the changes in code
+
+
+// 2. add the code
+// git add . (git and add all)
+or
+  // git add <filepath>
+
+
+  // 3.git commit -m "feature name"
+
+  // 4 git push
+
